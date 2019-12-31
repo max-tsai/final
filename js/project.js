2cefc029431e917d316c55ba0ef0ab457b4b0bd2
max-tsai <57138662+max-tsai@users.noreply.github.com>
2019-12-31 11:47:30 +0800

    Update project.js

--- js/project.js
+++ js/project.js
@@ -6,6 +6,7 @@ $(() => {
 //玩家一的結果
 $('#P2start').on('click', () => {
     console.log('按到了喔');
+    $('#winning2').empty();
     var x = [];
     for (i = 0; i < 4; i++) {
         x[i] = Math.floor(Math.random() * 6) + 1;
@@ -36,8 +37,6 @@ $('#P2start').on('click', () => {
         score = x[0] + x[1]
         console.log(score);
         $('#output2').val(score)
-
-
     }
     if (bb == 5) {
         score = x[2] + x[3]
@@ -67,82 +66,62 @@ $('#P2start').on('click', () => {
         console.log(restart);
         $('#output2').val(restart)
     }
-
-})
-
-//玩家2的結果
-$('#P3start').on('click', () => {
-    console.log('按到了喔');
-    var x = [];
-    for (i = 0; i < 4; i++) {
-        x[i] = Math.floor(Math.random() * 6) + 1;
+    var host = $('#output1').val();
+    var guset1 = $('#output2').val();
+    var diff = host - guset1
+    if (guset1.startsWith('一色') == 1 & host.startsWith('一色') == 0) {
+        var winning = "你贏了";
+        $('#winning2').val(winning);
     }
-    console.log(x);
-    $('#dice3').val(x);
-    x.sort();
-    var b0 = x[0] == x[1];
-    var b1 = x[1] == x[2];
-    var b2 = x[2] == x[3];
-    var bb = 0;
-    var bb = b0 + 2 * b1 + 4 * b2;
-
-    if (bb == 1) {
-        score = x[2] + x[3];
-        console.log(score);
-        $('#output3').val(score)
+    if (guset1.startsWith('一色') == 0 & host.startsWith('一色') == 1) {
+        var losing = "你輸了";
+        $('#winning2').val(losing);
     }
-    if (bb == 2) {
-        score = x[0] + x[3]
-        console.log(score);
-        $('#output3').val(score)
+    if (guset1.startsWith('一色') == 1 & host.startsWith('一色') == 1) {
+        var shost = parseInt(host.substr(3, 1))
+        var sguest = parseInt(guset1.substr(3, 1))
+        var sdiff = shost - sguest
 
+        if (sdiff > 0) {
+            var losing = "你輸了";
+            $('#winning2').val(losing);
+        }
+        if (sdiff < 0) {
+            var winning = "你贏了";
+            $('#winning2').val(winning);
+        }
+        if (sdiff == 0) {
+            var draw = "平手";
+            $('#winning2').val(draw);
+        }
     }
-
-    if (bb == 4) {
-        score = x[0] + x[1]
-        console.log(score);
-        $('#output3').val(score)
+    if (diff > 0) {
+        var losing = "你輸了";
+        $('#winning2').val(losing);
     }
-    if (bb == 5) {
-        score = x[2] + x[3]
-        console.log(score);
-        $('#output3').val(score)
-
+    if (diff < 0) {
+        var winning = "你贏了";
+        $('#winning2').val(winning);
     }
-    if (bb == 7) {
-        var str = "一色" + "  " + x[0];
-        console.log(str);
-        $('#output3').val(str)
-
-    }
-    if (bb == 0) {
-        var restart = "再試一次！";
-        console.log(restart);
-        $('#output3').val(restart)
-
-    }
-
-    if (bb == 3) {
-        var restart = "再試一次！";
-        console.log(restart);
-        $('#output3').val(restart)
-
-    }
-
-    if (bb == 6) {
-        var restart = "再試一次！";
-        console.log(restart);
-        $('#output3').val(restart)
+    if (diff == 0) {
+        var draw = "平手";
+        $('#winning2').val(draw);
     }
 
 })
+
+
 
 $('#P1start').on('click', () => {
     console.log('按到了喔');
+    $('#winning2').empty();
+    $('#output2').empty();
+    $('#dice2').empty();
+    
     var x = [];
     for (i = 0; i < 4; i++) {
         x[i] = Math.floor(Math.random() * 6) + 1;
-    }
+    };
 
     console.log(x);
     $('#dice1').val(x);
@@ -203,7 +182,6 @@ $('#P1start').on('click', () => {
         $('#output1').val(restart)
 
     }
-
     if (bb == 6) {
         var restart = "再試一次！";
         console.log(restart);
@@ -211,76 +189,5 @@ $('#P1start').on('click', () => {
     }
 
 
-
-
-})
-
-
-//判斷輸贏 P3
-$('#P3win').on('click', () => {
-    console.log('按到了喔');
-    var host = $('#output1').val();
-    var guest2 = $('#output3').val();
-    var diff2 = host - guest2
-
-    if (diff2 > 0) {
-        var losing = "你輸了";
-        $('#winning3').val(losing);
-    }
-    if (diff2 < 0) {
-        var winning = "你贏了";
-        $('#winning3').val(winning);
-    }
-    if (diff2 == 0) {
-        var draw = "平手";
-        $('#winning3').val(draw);
-    }
-})
-
-
-//判斷輸贏 P2
-$('#P2win').on('click', () => {
-    console.log('按到了喔');
-    var host = $('#output1').val();
-    var guset1 = $('#output2').val();
-    var diff = host - guset1
-    if (guset1.startsWith('一色') == 1 & host.startsWith('一色') == 0) {
-        var winning = "你贏了";
-        $('#winning2').val(winning);
-    }
-    if (guset1.startsWith('一色') == 0 & host.startsWith('一色') == 1) {
-        var losing = "你輸了";
-        $('#winning2').val(losing);
-    }
-    if (guset1.startsWith('一色') == 1 & host.startsWith('一色') == 1) {
-        var shost = parseInt(host.substr(3, 1))
-        var sguest = parseInt(guset1.substr(3, 1))
-        var sdiff = shost - sguest
-
-        if (sdiff > 0) {
-            var losing = "你輸了";
-            $('#winning2').val(losing);
-        }
-        if (sdiff < 0) {
-            var winning = "你贏了";
-            $('#winning2').val(winning);
-        }
-        if (sdiff == 0) {
-            var draw = "平手";
-            $('#winning2').val(draw);
-        }
-    }
-    if (diff > 0) {
-        var losing = "你輸了";
-        $('#winning2').val(losing);
-    }
-    if (diff < 0) {
-        var winning = "你贏了";
-        $('#winning2').val(winning);
-    }
-    if (diff == 0) {
-        var draw = "平手";
-        $('#winning2').val(draw);
-    }
 
 })
